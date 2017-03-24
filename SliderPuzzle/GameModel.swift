77500e031f18bf0a9646cf7b448ffffb3be25158
MAC <mac@macbook-pro-de-mac.local>
2017-03-25 00:46:48 +0100

    Randomizing the cell generation

--- SliderPuzzle/GameModel.swift
+++ SliderPuzzle/GameModel.swift
@@ -9,37 +9,67 @@
 import UIKit
 
 class GameModel: NSObject {
+
     
-    func createDataFromImage(gameSize: Int) -> [Cellinfo] {
+    // This method will take the image and slice it in parts for the matrix
+    func retrieveTilesData(image image: UIImage, into howMany: Int) -> [Cellinfo] {
+        let scale = Int(image.scale)
+        let width = Int(image.size.width / CGFloat(howMany))
+        let height = Int(image.size.height / CGFloat(howMany))
+        let cgImage = image.CGImage!
+        //The array with all the info
         var gameCells: [Cellinfo] = [Cellinfo]()
         
-        let originalImage: UIImage = UIImage(named: "hot-air-balloon")!
+        var adjustedHeight = height
         
-        
-        let cellSize = Int(originalImage.size.height) / (gameSize * 3)
-        
-        for i in 0...gameSize {
-            for p in 0...gameSize {
-                let cell:Cellinfo = Cellinfo()
-                cell.xIndex = i
-                cell.yIndex = p
-                let tmpImgRef: CGImage = originalImage.CGImage!
-                
-                let rect: CGImage = CGImageCreateWithImageInRect(tmpImgRef, CGRectMake(CGFloat(i * cellSize), CGFloat(p * cellSize), CGFloat(cellSize), CGFloat(cellSize)))!
-                
-                cell.image = UIImage(CGImage: rect)
-                gameCells.append(cell)
-                
+        var y = 0
+        for row in 0 ..< howMany {
+            if row == (howMany - 1) {
+                adjustedHeight = Int(image.size.height) - y
             }
+            var adjustedWidth = width
+            var x = 0
+            for column in 0 ..< howMany {
+                let cell:Cellinfo = Cellinfo()
+                cell.xIndex = row
+                cell.yIndex = column
+                
+                if column == (howMany - 1) {
+                    adjustedWidth = Int(image.size.width) - x
+                }
+                let origin = CGPoint(x: x * scale, y: y * scale)
+                let size = CGSize(width: adjustedWidth * scale, height: adjustedHeight * scale)
+                let tileCgImage = CGImageCreateWithImageInRect(cgImage, CGRect(origin: origin, size: size))!
+                
+                cell.image = UIImage(CGImage: tileCgImage, scale: image.scale, orientation: image.imageOrientation)
+                gameCells.append(cell)
+                x += width
+            }
+            y += height
         }
         
-        return gameCells
+        // I disorder the tiles before retrieving them
+        gameCells.shuffle()
         
+        return gameCells
     }
     
     
-    
 
+}
+
+
+// I've added an extension to the class Array to shuffle the elements.
+// I will make use of it when ordering the tiles
+extension Array {
+    mutating func shuffle() {
+        for i in 0 ..< (count - 1) {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            if i != j {
+                swap(&self[i], &self[j])
+            }
+        }
+    }
 }
 
 
--- SliderPuzzle/GameViewController.swift
+++ SliderPuzzle/GameViewController.swift
@@ -15,15 +15,20 @@ class GameViewController: UIViewController, UICollectionViewDelegate, UICollecti
     @IBOutlet weak var collectionView: UICollectionView!
     
     var gameCells: [Cellinfo] = [Cellinfo]()
+    var emptyCell: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Configuring the grid
+        // Configuring the grid
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
         
-        self.gameCells = self.gameModel.createDataFromImage(4)
+        
+        // Retrieve the data from the model to fill the cells
+        let originalImage: UIImage = UIImage(named: "hot-air-balloon")!
+        self.gameCells = self.gameModel.retrieveTilesData(image: originalImage, into: 4)
+        self.emptyCell = Int(arc4random_uniform(UInt32(self.gameCells.count)))
         
         // Register cell classes
         self.collectionView.registerNib(UINib(nibName: "GameCell", bundle: nil), forCellWithReuseIdentifier: "customGameCell")
@@ -50,7 +55,7 @@ class GameViewController: UIViewController, UICollectionViewDelegate, UICollecti
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         
         //Adjusting the size of cells to fill all the space
-        return CGSize(width: self.collectionView.frame.width/4, height: self.collectionView.frame.width/4);
+        return CGSize(width: self.collectionView.frame.width/4 - 6, height: self.collectionView.frame.width/4 - 6);
     }
     
     
@@ -60,17 +65,23 @@ class GameViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
     }
     
-    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let totalIndex:Int = indexPath.row + indexPath.section * 4
+        
+        // Filling the cells with the info and images
         
         let cell: GameCell = collectionView.dequeueReusableCellWithReuseIdentifier("customGameCell", forIndexPath: indexPath) as! GameCell
-        
-        var image : UIImage = self.gameCells[indexPath.row + indexPath.section * 4].image
+
+        let image : UIImage = self.gameCells[totalIndex].image
         
         cell.cellImage.image = image
-        cell.xIndex = self.gameCells[indexPath.row + indexPath.section * 4].xIndex
-        cell.yIndex = self.gameCells[indexPath.row + indexPath.section * 4].yIndex
         
+        if totalIndex == self.emptyCell {
+            cell.cellImage.alpha = 0
+        }
+        // This propierties will tell us the correct position of te tile
+        cell.xIndex = self.gameCells[totalIndex].xIndex
+        cell.yIndex = self.gameCells[totalIndex].yIndex
         
         
         
