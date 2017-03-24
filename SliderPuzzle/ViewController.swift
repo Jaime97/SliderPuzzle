5d2926aa273b1a71a001afcb5fdc2f6634b01c99
MAC <mac@macbook-pro-de-mac.local>
2017-03-24 13:17:21 +0100

    Initial Commit

---
+++ SliderPuzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SliderPuzzle
+//
+//  Created by MAC on 24/3/17.
+//  Copyright © 2017 Jaime Alcántara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
